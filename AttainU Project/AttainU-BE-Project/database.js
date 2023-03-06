55a929b93d5c7009b3619bcd09c50cb27c9b4eec
Aziza-Farooqui-au50 <azizafarooqui2@gmail.com>
2023-03-07 03:15:32 +0530

    .

--- AttainU Project/AttainU-BE-Project/database.js
+++ AttainU Project/AttainU-BE-Project/database.js
@@ -27,13 +27,18 @@ const orderSchema = new Schema({
 }
 
 
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { 
+    useNewUrlParser: true, 
+    useCreateIndex:true, 
+    useUnifiedTopology: true, 
+    useFindAndModify : true }).catch((err)=>{
+        console.log('Connection failed...')
+    });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
-}).catch(err => {
-    console.log('Connection failed...')
-});
+})
+    
 
 
 
